cbeb6879718f949836278772b7fed7b928d948f1
Alexandru Sima <alexsima203@gmail.com>
2023-11-08 17:58:33 +0200

    Change Playlist.user to contain an User instead of its username

--- src/main/audio/collections/Playlist.java
+++ src/main/audio/collections/Playlist.java
@@ -1,16 +1,17 @@
 package main.audio.collections;
 
+import main.User;
 import main.audio.files.AudioFile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Playlist {
-    private String user;
+    private final User user;
     private final boolean isPrivate = false;
     private final List<AudioFile> files = new ArrayList<>();
 
-    public Playlist(final String user) {
+    public Playlist(final User user) {
         this.user = user;
     }
 }
